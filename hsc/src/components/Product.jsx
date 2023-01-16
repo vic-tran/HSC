9e962c74ef936396e02259827031f1b58dca17af
Victor Tran <vc.tran998@gmail.com>
2023-01-15 23:22:11 -0800

    Dummy product list

--- hsc/src/components/Product.jsx
+++ hsc/src/components/Product.jsx
@@ -1,13 +1,76 @@
 import styled from "styled-components";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-const Container = styled.div``
 
-function Product() {
+const Container = styled.div`
+    flex:1;
+    margin: 5px;
+    min-width: 280x;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
+`
+
+const Circle = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+`
+
+const Image = styled.img`
+    height: 75%;
+    z-index: 2;
+`
+
+const Info = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: gray;
+    z-index:3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+`
+
+const Product = ({item}) => {
   return (
     <Container>
-    
+        <Circle />
+        <Image src={item.img} />
+        <Info>
+            <Icon>
+                <ShoppingCartIcon />
+            </Icon>
+            <Icon>
+                <SearchIcon />
+            </Icon>
+            <Icon>
+                <FavoriteBorderIcon />
+            </Icon>
+        </Info>
     </Container>
   )
 }
 
-export default Product
+export default Product;
--- hsc/src/pages/Home.jsx
+++ hsc/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from '../components/Categories'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
 import Slider from '../components/Slider'
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
         <Navbar />
         <Slider />
         <Categories />
+        <Products />
         <Newsletter />
         <Footer />
     </div>
