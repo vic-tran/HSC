ceb5fcef03203d409f2fd0f3fb5c70790301492e
Victor Tran <vc.tran998@gmail.com>
2023-01-16 17:35:04 -0800

    Ecommerce single product page add

--- hsc/src/App.js
+++ hsc/src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 
 const App = () => {
   return (
-  <ProductList />
+  <Product />
   )
 };
 
---
+++ hsc/src/pages/Product.jsx
@@ -0,0 +1,171 @@
+import styled from "styled-components";
+import Announcements from "../components/Announcements";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Container = styled.div`
+
+`;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+    font-size: 50px;
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+    font-weight: 50;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`; 
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+
+const FilterSizeOption = styled.option`
+
+`;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 200;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 3px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover {
+        background-color: #f8f4f4;
+    }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcements />
+        <Wrapper>
+            <ImgContainer>
+            <Image src="https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22ss/img/products/contentsArea_itemimg_16.jpg"/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Crew Neck Short Sleeve T-Shirt</Title>
+                <Desc>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Desc>
+                <Price>$19.99</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="red"/>
+                        <FilterColor color="black"/>
+                        <FilterColor color="gray"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <AddIcon />
+                        <Amount>1</Amount>
+                        <RemoveIcon />
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter />
+        <Footer />
+    </Container>
+  )
+}
+
+export default Product
