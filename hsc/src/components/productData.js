98412325a03a61379f137fcf6d85f4e34b405318
Victor Tran <vc.tran998@gmail.com>
2023-02-15 14:10:04 -0800

    Added dummy data to productlist

--- hsc/src/components/productData.js
+++ hsc/src/components/productData.js
@@ -23,6 +23,22 @@ const popularProducts = [
         id:6,
         img:"https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22ss/img/products/contentsArea_itemimg_16.jpg",
       },
+      {
+        id:7,
+        img:"https://cdn.shopify.com/s/files/1/0010/7538/0279/products/mens-premium-tee-talentless-steel-grey-s-211387.jpg?v=1635423553",
+      },
+      {
+        id:8,
+        img:"http://cdn.shopify.com/s/files/1/0783/3649/products/M703YCRO-1_4158c952-36e3-4e4b-b0_grande.jpg?v=1669939882",
+      },
+      {
+        id:9,
+        img:"https://i5.walmartimages.com/asr/1d883d1c-d422-46df-8746-da7439c62a09.3d7181ea11e2dc474b10f8d9dbea1584.jpeg",
+      },
+      {
+        id:10,
+        img:"https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-hoodie-ready-to-wear--HIY48WNPH002_PM2_Front%20view.jpg",
+      },
 ];
 
 export default popularProducts;
